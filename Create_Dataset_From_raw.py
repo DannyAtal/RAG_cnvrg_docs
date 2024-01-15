27ad32950a246d9d4f10f88165bbfa0ca2106cc7
DannyAtal <95615130+DannyAtal@users.noreply.github.com>
2024-01-15 11:28:52 +0200

    Update Create_Dataset_From_raw.py

--- Create_Dataset_From_raw.py
+++ Create_Dataset_From_raw.py
@@ -1,16 +1,9 @@
-#Import libraries
+# Import libraries
 from tika import parser
 from wcmatch import wcmatch
 import os
 from tqdm import tqdm
 import argparse
-
-
-parser = argparse.ArgumentParser(description="A script to extract data from .md files and prepare it as a txt file for embedding(aka. RAG).")
-parser.add_argument("--folder_path", type=str, help="Folder Path for the files to change")
-parser.add_argument("--folder_destination",default = "", type=str, help="output folder of the files")
-parser.add_argument("--dataset_name",default = "cnvrg_embedding_dataset.txt", type=str, help="created Dataset name .txt")
-args = parser.parse_args()
 
 # Create function to extract the data
 def extract_data(file_path):
@@ -21,12 +14,11 @@ def extract_data(file_path):
     except Exception as e:
         text = 'Error extracting contents!'
         return text
-    
-    
+
 # Create function to extract file name without path and extensions
 def extract_filename(file_path):
     file_name, _ = os.path.splitext(file_path)
-    return file_name.split("\\")[-1]
+    return os.path.split(file_name)[-1]
 
 # Create function to analyse folder path to get files for processing
 def analyse_path(folder_path, file_ext, folder_destination):
@@ -36,37 +28,34 @@ def analyse_path(folder_path, file_ext, folder_destination):
     print(str(len(files)) + " files to be processed!")
 
     # Loop through and process each file at a time
-    i = 0
-    while i < len(files):
-        text = extract_data(files[i])
+    for file_path in files:
+        text = extract_data(file_path)
 
         if text == "Error extracting contents!":
-            # Get a list of documents which had errors extracting the contents
-            error_files.append(files[i])
+            # Get a list of documents that had errors extracting the contents
+            error_files.append(file_path)
         else:
             # Save contents as a text file
             try:
-                file_name = extract_filename(files[i])
-                f = open(folder_destination + file_name + ".txt", "wb")
-                f.write(text.encode('utf-8'))
-                f.close()
+                file_name = extract_filename(file_path)
+                # Use os.path.join for constructing file paths
+                file_destination = os.path.join(folder_destination, file_name + ".txt")
+                with open(file_destination, "w", encoding='utf-8') as f:
+                    f.write(text)
             except Exception as e:
-                print (files[i])
-                print ("Error saving extraction to file " + str(e))
-
-        # Increment counter
-        i += 1
+                print(file_path)
+                print("Error saving extraction to file " + str(e))
 
     print(" The following files could not be processed: ")
     [print(x) for x in error_files]
-    
+
 # Create required variables
 # Read from the following path
-folder_path = args.folder_path
-#Only process the following file formats
+folder_path = '/cnvrg/Data/'
+# Only process the following file formats
 file_ext = "*.doc|*.docx|*.xlsx|*.xls|*.pdf|*.txt|*.ppt|*.pptx|*.md"
 # Save to the following path
-folder_destination = args.folder_destination
+folder_destination = '/cnvrg/Data/'
 
 # Run the extraction program
 analyse_path(folder_path, file_ext, folder_destination)
@@ -85,12 +74,12 @@ def extract_sentences(file_path):
         return final_content
 
     # Read file and remove empty line and new lines
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r', encoding='utf-8') as file:
         for line in file.readlines():
             if line.strip():
                 if len(line.strip()) > 2:
-                    content += line.replace('\n','')
-    
+                    content += line.replace('\n', '')
+
     # Create list of words and generate number of words
     split_content = content.split()
     seq_len = len(split_content)
@@ -98,8 +87,8 @@ def extract_sentences(file_path):
     # Check that contents have been extracted and reset Empty flag
     if seq_len > 0:
         final_content = ""
-    
-    # Create the sequences 
+
+    # Create the sequences
     for begin_loc in tqdm(range(0, seq_len, stride)):
         end_loc = min(begin_loc + stride, seq_len)
         if len(split_content[begin_loc:end_loc]) != 0:
@@ -121,36 +110,30 @@ def create_dataset(folder_path, file_ext, folder_destination, dataset_name="data
     print(str(len(files)) + " files to be processed!")
 
     # Loop through and process each file
-    i = 0
-    while i < len(files):
+    for file_path in files:
         try:
             # Get extracted sentences
-            contents = extract_sentences(files[i])
+            contents = extract_sentences(file_path)
             # Ignore "Empty" sentences
             if contents != 'Empty':
-                # Open or create dataset in append and byte mode
-                f = open(folder_destination + dataset_name, "ab")
-                # Save contents in utf-8 encoding
-                f.write(contents.encode('utf-8'))
-                f.close()
+                # Use os.path.join for constructing file paths
+                file_destination = os.path.join(folder_destination, dataset_name)
+                with open(file_destination, "a", encoding='utf-8') as f:
+                    f.write(contents)
         except Exception as e:
             # Log any issues encountered for further investigation
-            print(files[i])
-            print ("Error saving extraction to file " + str(e))
-
-        # Increment counter
-        i += 1
+            print(file_path)
+            print("Error saving extraction to file " + str(e))
 
     print("Finished processing ...")
-    
-    
+
 # Read from the following path
-folder_path = args.folder_path
-#Only process the following file formats
+folder_path = '/cnvrg/Data/'
+# Only process the following file formats
 file_ext = "*.txt"
 # Save to the following path
-folder_destination = args.folder_destination
+folder_destination = '/cnvrg/Data/'
 # Dataset name (can be omitted to use default values)
-dataset_name=args.dataset_name
+dataset_name = '/cnvrg/Data/cnvrg_embedding_dataset.txt'
 
 create_dataset(folder_path, file_ext, folder_destination, dataset_name)
